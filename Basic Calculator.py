0defac4a7c62f42e2b4ecb275a47f611f9e4cd9e
Mohamedshafei00 <mohamedshafei00@gmail.com>
2024-08-12 16:07:02 +0300

    Code commit

---
+++ Basic Calculator.py
@@ -0,0 +1,22 @@
+#input numbers
+
+number1= float(input("Enter First Number: "))
+number2= float(input("Enter Second Number: "))
+
+#Basic Operations
+
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+quotient = number1 / number2
+remainder = number1 % number2
+root = number1 ** (1/number2)
+power = number1 ** number2
+
+# Output
+print("Sum = ",sum)
+print("Difference = ",difference)
+print("Product = ",product)
+print("Remainder = ",remainder)
+print("Root = ",root)
+print("Power = ",power)
